4e67fed66f7276b304bc0b0b79fa5d9c58d1a7dc
daitx19950602 <dai.t@husky.neu.edu>
2018-10-06 17:52:58 -0400

    Update 2018-10-03
    Add Insert() Search() StartsWith() function for Trie

--- Public/DataStructure/Trie.java
+++ Public/DataStructure/Trie.java
@@ -25,4 +25,79 @@ public class Trie {
 
         isEndOfWord = false;
     }
+
+    /*
+    * @param word: a word
+    * @return: nothing
+    */
+    public void insert(String word) {
+        // write your code here
+
+        Trie cur = this;
+
+        for (char c : word.toCharArray())
+        {
+            int index = c - 'a';
+            if (cur.children[index] == null)
+            {
+                cur.children[index] = new Trie();
+                cur = cur.children[index];
+            } else
+            {
+                cur = cur.children[index];
+            }
+        }
+
+        cur.isEndOfWord = true;
+    }
+
+    /*
+     * @param word: A string
+     * @return: if the word is in the trie.
+     */
+    public boolean search(String word) {
+        // write your code here
+
+        Trie cur = this;
+
+        for (char c : word.toCharArray())
+        {
+            int index = c - 'a';
+            if (cur.children[index] == null)
+            {
+                return false;
+            } else
+            {
+                cur = cur.children[index];
+            }
+        }
+
+        return (cur != null && cur.isEndOfWord == true);
+
+    }
+
+    /*
+     * @param prefix: A string
+     * @return: if there is any word in the trie that starts with the given prefix.
+     */
+    public boolean startsWith(String prefix) {
+        // write your code here
+
+        Trie cur = this;
+
+        for (char c : prefix.toCharArray())
+        {
+            int index = c - 'a';
+            if (cur.children[index] == null)
+            {
+                return false;
+            } else
+            {
+                cur = cur.children[index];
+            }
+        }
+
+        return true;
+    }
+
 }
